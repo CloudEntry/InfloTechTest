5b8f52f59bf90c9505fde753371d070451bab4ca
Jack Gee <jack.gee@protonmail.com>
2023-11-10 08:45:25 +0000

    Added Logs page

---
+++ UserManagement.Services/Implementations/LogService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using UserManagement.Data;
+using UserManagement.Models;
+using UserManagement.Services.Domain.Interfaces;
+
+namespace UserManagement.Services.Domain.Implementations;
+
+public class LogService : ILogService
+{
+    private readonly IDataContext _dataAccess;
+    public LogService(IDataContext dataAccess) => _dataAccess = dataAccess;
+
+    public IEnumerable<Log> GetLog(int id) => _dataAccess.GetAll<Log>().AsNoTracking().Where(x => x.LogId == id);
+
+    public IEnumerable<Log> GetAll() => _dataAccess.GetAll<Log>();
+
+    public void Create(Log log) => _dataAccess.Create(log);
+
+    public void Delete(Log log) => _dataAccess.Delete(log);
+    public void Update(Log log) => _dataAccess.Update(log);
+}
---
+++ UserManagement.Services/Interfaces/ILogService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UserManagement.Models;
+
+namespace UserManagement.Services.Domain.Interfaces;
+
+public interface ILogService
+{
+    // IEnumerable<User> FilterByActive(bool isActive);
+    IEnumerable<Log> GetLog(int id);
+    IEnumerable<Log> GetAll();
+    void Create(Log log);
+    void Delete(Log log);
+    void Update(Log log);
+}
